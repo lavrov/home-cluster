9b65c2f505f6b3764650cd8dc7be562f3c741e87
Vitaly Lavrov <vitaly.lavrov@coralogix.com>
2022-12-20 23:26:53 +0100

    Add ksops

--- flake.nix
+++ flake.nix
@@ -11,7 +11,6 @@
             url = "https://github.com/mozilla/sops/releases/download/v${version}/sops-v${version}.darwin.arm64";
             sha256 = "vpziZcfz07U0U10qXve0FgC/K4JBsaT5XkiATSBiiy4=";
           };
-          dontUnpack = true;
           phases = ["installPhase"];
           installPhase = ''
             mkdir -p $out/bin
@@ -28,11 +27,29 @@
             url = "https://github.com/goabout/kustomize-sopssecretgenerator/releases/download/v${version}/SopsSecretGenerator_${version}_darwin_arm64";
             sha256 = "PhzzZw1nESbRY99YD5dYALxkcQtUQBNL++qe4Ckdq18=";
           };
-          dontUnpack = true;
           phases = ["installPhase"];
           installPhase = ''
             mkdir -p $out/bin
             cp $src $out/bin/${pname}
+            chmod +x $out/bin/${pname}
+          '';
+          inherit system;
+        };
+      # wait for it to support KRM functions
+      # https://github.com/viaduct-ai/kustomize-sops/issues/141
+      ksops =
+        pkgs.stdenv.mkDerivation rec {
+          pname = "ksops";
+          version = "3.0.2";
+          src = pkgs.fetchurl {
+            url = "https://github.com/viaduct-ai/kustomize-sops/releases/download/v${version}/ksops_${version}_Darwin_arm64.tar.gz";
+            sha256 = "wMveLlD9xx7mceMRFHKYPQeLfptTAXvgcCTfes09b40=";
+          };
+          phases = ["unpackPhase" "installPhase"];
+          setSourceRoot = "sourceRoot=`pwd`";
+          installPhase = ''
+            mkdir -p $out/bin
+            cp ksops $out/bin/${pname}
             chmod +x $out/bin/${pname}
           '';
           inherit system;
@@ -53,6 +70,7 @@
             pkgs.kubectl
             sops
             sops-secret-generator
+            ksops
             # custom helper commands
             apply-config
             render-config
